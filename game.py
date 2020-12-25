799e06412baec1e44b75cec589d2a59deb39e41a
Tomas Lopes <tomas050302@gmail.com>
2020-12-25 13:14:19 +0000

    feat: added game loop

--- game.py
+++ game.py
@@ -7,27 +7,65 @@ max_bias = 1
 
 def play(json_file_path):
     clear_console()
+
+    print('Entrou no modo jogo.')
+    print('Para sair deste modo introduza -1 no valor da sua resposta.')
+    input('Pressione ENTER quando estiver pronto para começar...')
+
     try:
         with open(json_file_path, 'r') as file:
             json_obj = json.load(file)
 
-        competition = generate_random_object(json_obj)
-        game = generate_random_object(competition['games'])
+        points = 0
+        correct = 0
 
-        options = generate_options(game)
+        while(correct != -1):
+            clear_console()
+            correct = game(json_obj)
+            if(correct == 1):
+                points += 1
+                continue
 
-        question_str = elaborate_question(competition, game, options)
-
-        print(question_str)
-
-        try:
-            answer = input('\nSelecione a opção correta: ')
-        except ValueError:
-            print('Selecione uma opção de valor numérico')
+            if(correct != -1):
+                input('\nPressione a tecla ENTER passar à próxima pergunta...')
 
     except FileNotFoundError:
         print('Ocorreu um erro na leitura do ficheiro JSON')
         print('Verifique se o ficheiro já foi criado.')
+
+
+def game(json_obj):
+    competition = generate_random_object(json_obj)
+    game = generate_random_object(competition['games'])
+
+    options = generate_options(game)
+
+    correct_answer_index = get_correct_answer_index(options)
+
+    question_str = elaborate_question(competition, game, options)
+
+    print(question_str)
+
+    try:
+        answer = int(input('\nSelecione a opção correta: '))
+
+        if((answer < 1 or answer > n_of_options_per_question) and answer != -1):
+            print('Selecione uma opção válida.')
+            return
+        elif(answer == -1):  # Code to exit the game
+            return -1
+
+        if(answer - 1 == correct_answer_index):
+            print('\n\nCorreto.')
+            return 1
+        else:
+            correct_answer = options[correct_answer_index]['question']
+            print(
+                f'\n\nErrado.\nA opção correta era a opção {correct_answer_index + 1}. {correct_answer}')
+
+            return 0
+    except ValueError:
+        print('Selecione uma opção de valor numérico')
 
 
 def generate_random_object(data):
@@ -44,7 +82,7 @@ def generate_options(game):
 
     correct = f'{home_score}-{away_score}'
 
-    options.append(correct)
+    options.append({'correct': True, 'question': correct})
 
     i = 1
 
@@ -54,7 +92,7 @@ def generate_options(game):
         if(new_option in options):
             continue
         else:
-            options.append(new_option)
+            options.append({'correct': False, 'question': new_option})
             i += 1
 
     import random
@@ -101,10 +139,22 @@ def elaborate_question(competition, game, options):
     i = 0
 
     for option in options:
-        options_str += f'{i+1}. {option}    '
+        question = option['question']
+
+        options_str += f'{i+1}. {question}    '
         i += 1
 
     return f'Ano: {year}\nLocal: {host}\nJogo entre: {home_team} x {away_team}\n\n Resultado:\n\n {options_str}'
+
+
+def get_correct_answer_index(options):
+    i = 0
+
+    for option in options:
+        if(option['correct']):
+            return i
+
+        i += 1
 
 
 def clear_console():
